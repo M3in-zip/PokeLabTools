a8ea4f8dae1188ff74ccaad9d9b4e3c9a945effe
lorenzo-giovanardi <myp.lore@gmail.com>
2026-01-11 00:08:28 +0100

    chore: refactor

--- src/api/pokemon.ts
+++ src/api/pokemon.ts
@@ -1,3 +1,17 @@
+const baseUrl = "https://pokeapi.co/api/v2";
+
 export const fetchPokemonBaseList = async () => {
-    
-}
+  const res = await fetch("https://pokeapi.co/api/v2/pokemon?limit=2000");
+  if (!res.ok) {
+    throw new Error("Errore nel fetch");
+  }
+  return res.json();
+};
+
+export const fetchPokemonByName = async (name: string) => {
+  const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`);
+  if (!res.ok) {
+    throw new Error("Pokémon non trovato");
+  }
+  return res.json();
+};
--- src/routes/about.tsx
+++ src/routes/about.tsx
@@ -1,44 +1,33 @@
-import { createFileRoute } from '@tanstack/react-router'
-import { useQuery } from '@tanstack/react-query'
-import { useEffect } from 'react'
+import { createFileRoute } from "@tanstack/react-router";
+import { useQuery } from "@tanstack/react-query";
+import { useEffect } from "react";
+import { fetchPokemonBaseList } from "@/api/pokemon.ts";
+import { usePokemonStore } from "@/stores/pokemonStore";
 
-export const Route = createFileRoute('/about')({
+export const Route = createFileRoute("/about")({
   component: About,
-})
-
-// funzione di fetch
-const fetchPokemon = async () => {
-  const res = await fetch('https://pokeapi.co/api/v2/pokemon?limit=2000')
-  if (!res.ok) {
-    throw new Error('Errore nel fetch')
-  }
-  return res.json()
-}
-
-const fetchPokemonByName = async (name: string) => {
-  const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`)
-  if (!res.ok) {
-    throw new Error('Pokémon non trovato')
-  }
-  return res.json()
-}
-
+});
 
 function About() {
-  const { data, isLoading, error } = useQuery({
-    queryKey: ['pokemon'],
-    queryFn: () => /* fetchPokemonByName("regice") */fetchPokemon(),
-  })
-  
-  useEffect(() => {console.log("api v2/pokemon",data)}, [data])
+  const setPokemonList = usePokemonStore((state) => state.setPokemonList);
 
-  if (isLoading) return <div className="p-2">Loading...</div>
-  if (error) return <div className="p-2">Errore nel caricamento</div>
+  const { data, isLoading, error } = useQuery({
+    queryKey: ["pokemonList"],
+    queryFn: () => fetchPokemonBaseList(),
+  });
+
+  useEffect(() => {
+    console.log("useEffect data api: ", data);
+    if (data) setPokemonList(data);
+  }, [data]);
+
+  if (isLoading) return <div className="p-2">Loading...</div>;
+  if (error) return <div className="p-2">Errore nel caricamento</div>;
 
   return (
     <div className="p-2">
       <h1 className="text-xl font-bold mb-2">Pokémon</h1>
       {/* <img src={data.sprites.front_default} alt={data.name} className="w-48 h-48 pixel-art" /> */}
     </div>
-  )
+  );
 }
