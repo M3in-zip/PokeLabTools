67333412b3188de14fe5b48a2b81814179fd5bf8
lorenzo-giovanardi <myp.lore@gmail.com>
2026-01-17 12:03:08 +0100

    data: add moves.json

--- src/components/pokemon-build/pokemon-build.tsx
+++ src/components/pokemon-build/pokemon-build.tsx
@@ -11,7 +11,8 @@ interface PokemonBuildProps {
 
 export const PokemonBuild = ({ title }: PokemonBuildProps) => {
   const [selectedPokemon, setSelectedPokemon] = useState<string>("rayquaza");
-  const [baseStats, setBaseStats] = useState<number[]>([105, 150, 90, 150, 90, 95]);  
+  const [baseStats, setBaseStats] = useState<number[]>([105, 150, 90, 150, 90, 95]);
+  const [moves, setMoves] = useState<number[]>([])
 
   const { data, isLoading, error } = useQuery({
     queryKey: ["pokemonData", selectedPokemon],
@@ -24,7 +25,10 @@ export const PokemonBuild = ({ title }: PokemonBuildProps) => {
   useEffect(() => {
     if (data) {
       const stats = data.stats.map((stat: any) => stat.base_stat);
+      const movesNames = data.moves.map((move:{move:{name:string}}) => move.move.name);
       setBaseStats(stats);
+      setMoves(movesNames)
+      if(data) console.log("pokemon data: ", data)
     }
   }, [data]);
 
